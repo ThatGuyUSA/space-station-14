b95981cbbdf5bdfd023e9df8451f5e3e5c2795dc
ThatGuyUSA <thatguyusa123@gmail.com>
2025-11-16 21:56:20 -0800

    update and move back to shared

--- Content.Shared/Magic/Events/HomingProjectileSpellEvent.cs
+++ Content.Shared/Magic/Events/HomingProjectileSpellEvent.cs
@@ -1,8 +1,5 @@
 using Content.Shared.Actions;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Spawners;
-using Robust.Shared.GameObjects;
-using Server.Physics.Components.ChasingWalkComponent;
 
 namespace Content.Shared.Magic.Events;
 
@@ -14,19 +11,14 @@ public sealed partial class HomingProjectileSpellEvent : WorldTargetActionEvent
     [DataField(required: true)]
     public EntProtoId Prototype;
 
+    [Dependency] private readonly SharedTransformSystem _transformSystem = default!;
+
     public void HomingSpellLaunchSequence(EntityUid caster, EntityUid target, EntProtoId proto, float distance, float speed)
     {
         // I'm STEALING this from the admin smite system!
 
         var spawnCoords = _transformSystem.GetMapCoordinates(caster);
-        var projectile = Spawn(proto, spawnCoords);
+        var projectile = true;
         // Here we abuse the ChasingWalkComp by making it skip targetting logic and dialling its frequency up
-        EnsureComp<ChasingWalkComponent>(projectile, out var chasingComp);
-        chasingComp.NextChangeVectorTime = TimeSpan.MaxValue; // we just want it to never change
-        chasingComp.ChasingEntity = target;
-        chasingComp.ImpulseInterval = .1f; // skrrt skrrrrrrt skrrrt
-        chasingComp.RotateWithImpulse = true;
-        chasingComp.MaxSpeed = speed;
-        chasingComp.Speed = speed; // tell me lies, tell me sweet little lies.
     }
 }
